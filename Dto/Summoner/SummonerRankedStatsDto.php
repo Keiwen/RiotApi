55537ce4c1ee86722b0806507b4c5d5e688721e3
Keiwen <romainwald@hotmail.com>
2016-11-07 15:32:00 +0100

    big int to string

--- Dto/Summoner/SummonerRankedStatsDto.php
+++ Dto/Summoner/SummonerRankedStatsDto.php
@@ -42,11 +42,11 @@ class SummonerRankedStatsDto extends DtoParent
 
     /**
      * Date stats were last modified specified as epoch milliseconds.
-     * @return int
+     * @return string
      */
     public function getModifyDate()
     {
-        return $this->get('modifyDate', 0);
+        return $this->get('modifyDate', '0');
     }
 
 
@@ -54,18 +54,19 @@ class SummonerRankedStatsDto extends DtoParent
      * @param int $modifyDate
      * @return static
      */
-    public function setModifyDate(int $modifyDate)
+    public function setModifyDate($modifyDate)
     {
-        return $this->set('modifyDate', $modifyDate);
+        //if int to large (millisec timestamp), turned to float, so always turned to string
+        return $this->set('modifyDate', ''.$modifyDate);
     }
 
     /**
      * Summoner ID.
-     * @return int
+     * @return string
      */
     public function getSummonerId()
     {
-        return $this->get('summonerId', 0);
+        return $this->get('summonerId', '0');
     }
 
 
@@ -73,9 +74,10 @@ class SummonerRankedStatsDto extends DtoParent
      * @param int $summonerId
      * @return static
      */
-    public function setSummonerId(int $summonerId)
+    public function setSummonerId($summonerId)
     {
-        return $this->set('summonerId', $summonerId);
+        //if int to large, turned to float, so always turned to string
+        return $this->set('summonerId', ''.$summonerId);
     }
 
 
